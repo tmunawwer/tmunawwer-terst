fc4aab66c6c1b8a72c5a17a758b437fc16d751ac
tmunawwer <37419565+tmunawwer@users.noreply.github.com>
2018-06-18 23:08:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,40 +11,6 @@ client.on('ready', () => {
     client.user.setStatus("Offline");
 });
 
-//google
-let prefix;
-const commands = client.commands = require('./commands');
-
-client.on('MESSAGE_CREATE', (message, shard_id) => {
-  if (message.author.bot || !prefix || !prefix.test(message.content)) return;
-  const raven_context = {
-    message,
-    command: null,
-  };
-  try {
-    let [command, ...args] = message.content.replace(prefix, '').trim().split(' ');
-    if (!command) return;
-    message.content = args.join(' ');
-    if (command in commands) {
-      command = commands[command];
-      if (command.owner && message.author.id !== '173547401905176585') return;
-    } else {
-      message.content = `${command} ${message.content}`;
-      command = commands.search;
-    }
-    raven_context.command = { command: command.name, args };
-    logger.log('COMMAND', shard_id, command.name);
-    command(message);
-    client.stats.increment(`commands.${command.name}`);
-  } catch (err) {
-    const event = client.raven.captureException(err, {
-      extra: raven_context,
-    });
-    logger.log('SENTRY EXCEPTION', event);
-  }
-});
-//google end
-
 client.on('message', message => {
     ans = message.content;
     if (message.content === 'ping') {
